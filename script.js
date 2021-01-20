80567c304b35c023879dab087e892c238a615756
Ariel Marcus <ajmarcus@users.noreply.github.com>
2021-01-19 20:10:34 -0700

    submit form via ajax

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ $('#foot').load('footer.html');
 
 $('#send').on('click', function(e) {
   e.preventDefault();
+  const form = new FormData(document.getElementById('email'))
+  await fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(form).toString()
+  });
   const $name = $('#item');
   $newP = $(`<p>Thank you ${$name.val()}. I'll be in touch soon.</p>`);
   $('#send').after($newP);
   $("#email").trigger("reset"); //reset form in jQuery.//
-  
 });
